49d9f83f2d4c02fbf9237e5d62788a32d7ecfde4
Timothy Shores <timothymshores@gmail.com>
2019-02-07 11:47:26 -0600

    Finished implementing a slider component using Swiper

--- script.js
+++ script.js
@@ -27,3 +27,20 @@ window.onscroll = function () {
         navBar.style.background = "none";
     }
 };
+
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})

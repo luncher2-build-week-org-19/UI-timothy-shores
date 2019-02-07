6578fa5554dd8887f27230a50536f3828539ebde
Timothy Shores <timothymshores@gmail.com>
2019-02-07 09:40:15 -0600

    Updated navbar and body margins

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-// const navbar = document.getElementsByClassName('navbar')[0];
-// window.onscroll = function () {
-//     // "use strict";
-//     if (document.body.scrollTop >= 200) {
-//         navbar.classList.add("nav-colored");
-//         navbar.classList.remove("nav-transparent");
-//     }
-//     else {
-//         navbar.classList.add("nav-transparent");
-//         navbar.classList.remove("nav-colored");
-//     }
-// };
-
 const navBar = document.getElementsByClassName("navbar")[0];
 const h1 = document.getElementsByTagName("h1")[0];
 const search = document.getElementById("search");
-const hamburger = document.getElementById("hamburger");
+const signIn = document.getElementById("nav-item3");
+const donate = document.getElementById("nav-item4");
+const discover = document.getElementById("nav-item1A");
 
-window.onscroll = function () { scrollFunction() };
+
+
+window.onscroll = function () {
+    scrollFunction()
+};
 
 function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        navBar.classList.add("nav-colored");
+        navBar.style.background = "white";
         h1.style.color = "black";
         search.style.color = "black";
         hamburger.style.color = "black";
+        signIn.style.color = "black";
+        donate.style.color = "black";
+        discover.style.color = "black";
     } else {
         navBar.classList.remove("nav-colored");
         h1.style.color = "white";
         search.style.color = "white";
         hamburger.style.color = "white";
+        signIn.style.color = "white";
+        donate.style.color = "white";
+        discover.style.color = "white";
+        navBar.style.background = "none";
     }
 }
